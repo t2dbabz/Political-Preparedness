2ba0471b6527c271e9d78a347d0da7997a99fbac
Babatunde Owoleke <tundeowoleke1@gmail.com>
2022-02-20 19:44:37 +0100

    Implement Glide for image loading

--- app/src/main/java/com/example/android/politicalpreparedness/ui/representative/adapter/RepresentativeBindingAdapters.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/ui/representative/adapter/RepresentativeBindingAdapters.kt
@@ -5,12 +5,24 @@ import android.widget.ImageView
 import android.widget.Spinner
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestOptions
+import com.example.android.politicalpreparedness.R
 
 @BindingAdapter("profileImage")
 fun fetchImage(view: ImageView, src: String?) {
     src?.let {
         val uri = src.toUri().buildUpon().scheme("https").build()
         //TODO: Add Glide call to load image and circle crop - user ic_profile as a placeholder and for errors.
+        Glide.with(view)
+            .load(uri)
+            .apply(RequestOptions()
+                .placeholder(R.drawable.ic_profile)
+                .error(R.drawable.ic_profile)
+                .circleCrop())
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .into(view)
     }
 }
 
